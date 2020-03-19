761e86bcd2c81be3d1f440511a19bb2f80fb0e17
Sunnat DG <sunnat@deligram.com>
2020-03-19 13:58:21 +0600

    environment-setup: implemented material design dependency, convert the theme into MaterialDesign, added a Material button in the layout for demo

--- app/src/main/java/dev/sunnat629/metarialdesignformediumblog/MainActivity.kt
+++ app/src/main/java/dev/sunnat629/metarialdesignformediumblog/MainActivity.kt
@@ -2,10 +2,10 @@ package dev.sunnat629.metarialdesignformediumblog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
